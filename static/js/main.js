dbb4f522cbf4a1cb79fc66f0a77cba69cabfe6c1
Corey <coreyarice@gmail.com>
2018-07-08 14:32:33 -0500

    Hide main nav on navigation

--- static/js/main.js
+++ static/js/main.js
@@ -6,3 +6,22 @@ $(function() {
         event.target.classList.add('active')
     });
 });
+
+$(function() {
+   var pastRoute = window.location.pathname.includes('2017')
+   if (pastRoute) {
+    $('.main-nav').each(function() {
+        this.classList.add('hidden')
+    })
+    $('.off-main-nav').each(function() {
+        this.classList.remove('hidden')
+    })
+   } else {
+    $('.main-nav').each(function() {
+        this.classList.remove('hidden')
+    })
+    $('.off-main-nav').each(function() {
+        this.classList.add('hidden')
+    })
+   }
+});
