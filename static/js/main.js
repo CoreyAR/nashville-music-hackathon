d4ebc9f13c8ae4d01d2a72522c344f368aa778e8
Corey <coreyarice@gmail.com>
2018-08-15 11:25:23 -0500

    Show modal on page load

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,33 @@
-$(function() {
-    $('.about-button').bind('click', function(event) {
-    	$('.about-button').each(function() {
-    		this.classList.remove('active')
-    	})
+$(function () {
+    $('.about-button').bind('click', function (event) {
+        $('.about-button').each(function () {
+            this.classList.remove('active')
+        })
         event.target.classList.add('active')
     });
 });
 
-$(function() {
-   var pastRoute = window.location.pathname.includes('2017')
-   if (pastRoute) {
-    $('.main-nav').each(function() {
-        this.classList.add('hidden')
-    })
-    $('.off-main-nav').each(function() {
-        this.classList.remove('hidden')
-    })
-   } else {
-    $('.main-nav').each(function() {
-        this.classList.remove('hidden')
-    })
-    $('.off-main-nav').each(function() {
-        this.classList.add('hidden')
-    })
-   }
+$(function () {
+    var pastRoute = window.location.pathname.includes('2017')
+    if (pastRoute) {
+        $('.main-nav').each(function () {
+            this.classList.add('hidden')
+        })
+        $('.off-main-nav').each(function () {
+            this.classList.remove('hidden')
+        })
+    } else {
+        $('.main-nav').each(function () {
+            this.classList.remove('hidden')
+        })
+        $('.off-main-nav').each(function () {
+            this.classList.add('hidden')
+        })
+    }
+});
+
+// Show modal on page load
+$(window).on('load', function () {
+    var hash = window.location.hash
+    $(hash).modal('show');
 });
